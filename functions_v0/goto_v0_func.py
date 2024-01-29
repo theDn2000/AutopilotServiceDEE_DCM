e9c543391d0394fd2aface418056a345dc8d6b87
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-01-29 20:50:19 +0100

    mavlink falta reparar telemetria, hacer modify parameters y geofence

--- functions_v0/goto_v0_func.py
+++ functions_v0/goto_v0_func.py
@@ -3,15 +3,16 @@ import math
 import threading
 import time
 import dronekit  # noqa: F401
+from pymavlink import mavutil
 from dronekit import connect, Command, VehicleMode  # noqa: F401
 
 
 def goto_trigger(self, internal_client, external_client, sending_topic):
-    print('Going to the waypoint')
+    print('- Autopilot Service: Going to the waypoint')
     self.reaching_waypoint = True
     lat = -35.3622286
     lon = 149.1650999
-    w = threading.Thread(target=self.goto_v0, args=[lat, lon, internal_client, external_client, sending_topic])
+    w = threading.Thread(target=self.goto_MAVLINK, args=[lat, lon, internal_client, external_client, sending_topic])
     w.start()
 
 
@@ -40,6 +41,26 @@ def goto_v0(self, lat, lon, internal_client, external_client, sending_topic):
     external_client.publish(sending_topic + "/destinationPointReached", json.dumps(destination_coordinates))
     self.reaching_waypoint = False
     print("Destination reached, message published")
+
+def goto_MAVLINK(self, lat, lon, alt, sending_topic):
+    self.vehicle.mav.send(
+        mavutil.mavlink.MAVLink_set_position_target_global_int_message(10, self.vehicle.target_system,
+                                                                       self.vehicle.target_component,
+                                                                       mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                                                                       int(0b110111111000), int(lat * 10 ** 7),
+                                                                       int(lon * 10 ** 7), alt, 0, 0, 0, 0, 0, 0, 0,
+                                                                       0))
+
+
+
+    dist = self._distanceToDestinationInMeters(lat ,lon)
+    distanceThreshold = 0.5
+    while dist > distanceThreshold:
+        time.sleep(0.25)
+        dist = self._distanceToDestinationInMeters(lat, lon)
+    self.lock.acquire()
+    self.client.publish(sending_topic + '/arrivedToPoint')
+    self.lock.release()
 
 
 def distanceInMeters(aLocation1, aLocation2):
