5e0c186bdc4e73dfc4ff80bbd7a7806d1145afeb
Daniel Carneros Mateu <109517814+theDn2000@users.noreply.github.com>
2024-06-06 11:17:55 +0200

    DashboardRemote: websocket ip changed

--- DashboardRemote.py
+++ DashboardRemote.py
@@ -65,7 +65,8 @@ class App(ctk.CTk):
 
         # WebSocket client parameters
         self.websocket = None
-        self.url = "ws://192.168.1.242:8765" # Replace localhost with the IP of the server
+        self.url = "ws://192.168.0.180:8765" # Replace localhost with the IP of the server
+        #self.url = "ws://localhost:8765" # Replace localhost with the IP of the server
         self.ws_connected = False
         self.loop = None
 
